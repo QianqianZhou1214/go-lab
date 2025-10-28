cc2597a3508959466be0a0a5ad0e2dad4b13c6cc
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-28 21:36:39 +0100

    Gin framework server impl

---
+++ cmd/server/server.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/gin-gonic/gin"
+)
+
+type GinServer interface {
+}
+
+type GinServerBuilder struct {
+}
+
+// This represents the Gin server
+type ginServer struct {
+	engine *gin.Engine // The engine for server
+	server *http.Server
+}
+
+// To initialize Gin server
+func NewGinServerBuilder() *GinServerBuilder {
+	return &GinServerBuilder{}
+}
+
+// Build Gin serve and return the instance with created engine
+func (b *GinServerBuilder) Build() GinServer {
+	engine := gin.Default()
+	return &ginServer{engine: engine}
+}
+
+// To start server
+func (gs *ginServer) Start(ctx context.Context, httpAddress string) error {
+	gs.server = &http.Server{
+		Addr:    httpAddress,
+		Handler: gs.engine,
+	}
+	go func() {
+		if err := gs.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logrus.Fatalf("listening: %s\n", err)
+		}
+	}()
+	logrus.Infof("Https server is running on port %s", httpAddress)
+	return nil
+}
